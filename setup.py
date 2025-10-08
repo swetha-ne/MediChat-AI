2a2802b9138b70fa71ff9a21fc318375ddc2dfe9
Swetha Nethmi <snethmi18@gmail.com>
2025-10-08 07:20:40 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="swetha",
+    author_email="swethane28@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
